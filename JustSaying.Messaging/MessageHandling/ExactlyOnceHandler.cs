492446050a2025db982b3dcec9e65914e6818575
Pawel Matyjasek <pawel.matyjasek@just-eat.com>
2015-01-14 10:34:36 +0000

    and remember to add right files to commits...

--- JustSaying.Messaging/MessageHandling/ExactlyOnceHandler.cs
+++ JustSaying.Messaging/MessageHandling/ExactlyOnceHandler.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Threading.Tasks;
 using JustSaying.Models;
 
 namespace JustSaying.Messaging.MessageHandling
@@ -27,43 +26,6 @@ namespace JustSaying.Messaging.MessageHandling
             try
             {
                 var successfullyHandled = _inner.Handle(message);
-                if (successfullyHandled)
-                {
-                    _messageLock.TryAquire(lockKey);
-                }
-                return successfullyHandled;
-            }
-            catch
-            {
-                _messageLock.Release(lockKey);
-                throw;
-            }
-        }
-    }
-
-    public class ExactlyOnceAsyncHandler<T> : IAsyncHandler<T> where T : Message
-    {
-        private readonly IAsyncHandler<T> _inner;
-        private readonly IMessageLock _messageLock;
-        private readonly int _timeOut;
-
-        public ExactlyOnceAsyncHandler(IAsyncHandler<T> inner, IMessageLock messageLock, int timeOut)
-        {
-            _inner = inner;
-            _messageLock = messageLock;
-            _timeOut = timeOut;
-        }
-
-        public async Task<bool> HandleAsync(T message)
-        {
-            var lockKey = string.Format("{0}-{1}-{2}", _inner.GetType().FullName.ToLower(), typeof(T).Name.ToLower(), message.UniqueKey());
-            bool canLock = _messageLock.TryAquire(lockKey, TimeSpan.FromSeconds(_timeOut));
-            if (!canLock)
-                return true;
-
-            try
-            {
-                var successfullyHandled = await _inner.HandleAsync(message);
                 if (successfullyHandled)
                 {
                     _messageLock.TryAquire(lockKey);
