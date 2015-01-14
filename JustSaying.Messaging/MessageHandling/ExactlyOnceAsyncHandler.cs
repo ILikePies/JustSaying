0dd6a789299356ada202fd88c61c3cfbbe5324c0
Pawel Matyjasek <pawel.matyjasek@just-eat.com>
2015-01-14 10:33:53 +0000

    start simple with all misc handlers before we start changing the guts of the library

--- JustSaying.Messaging/MessageHandling/ExactlyOnceAsyncHandler.cs
+++ JustSaying.Messaging/MessageHandling/ExactlyOnceAsyncHandler.cs
@@ -4,43 +4,6 @@ using JustSaying.Models;
 
 namespace JustSaying.Messaging.MessageHandling
 {
-    public class ExactlyOnceHandler<T> : IHandler<T> where T : Message
-    {
-        private readonly IHandler<T> _inner;
-        private readonly IMessageLock _messageLock;
-        private readonly int _timeOut;
-
-        public ExactlyOnceHandler(IHandler<T> inner, IMessageLock messageLock, int timeOut)
-        {
-            _inner = inner;
-            _messageLock = messageLock;
-            _timeOut = timeOut;
-        }
-        
-        public bool Handle(T message)
-        {
-            var lockKey = string.Format("{0}-{1}-{2}", _inner.GetType().FullName.ToLower(), typeof(T).Name.ToLower(), message.UniqueKey());
-            bool canLock = _messageLock.TryAquire(lockKey, TimeSpan.FromSeconds(_timeOut));
-            if (!canLock)
-                return true;
-
-            try
-            {
-                var successfullyHandled = _inner.Handle(message);
-                if (successfullyHandled)
-                {
-                    _messageLock.TryAquire(lockKey);
-                }
-                return successfullyHandled;
-            }
-            catch
-            {
-                _messageLock.Release(lockKey);
-                throw;
-            }
-        }
-    }
-
     public class ExactlyOnceAsyncHandler<T> : IAsyncHandler<T> where T : Message
     {
         private readonly IAsyncHandler<T> _inner;
